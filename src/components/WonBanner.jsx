41151ade0e4c0673c6241c4b0acfd8b75018e420
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-28 15:44:55 +0530

    fix

--- src/components/WonBanner.jsx
+++ src/components/WonBanner.jsx
@@ -7,9 +7,9 @@ const WonBanner = ({ setWon, resetGame }) => {
   };
 
   return (
-    <div className='w-screen z-10 absolute top-0 h-screen flex backdrop-blur-sm bg-opacity-70 bg-gray-900 justify-center items-center'>
-      <div className='bg-[#FFEB55] flex flex-col justify-center items-center bg-opacity-45 p-8 rounded-lg shadow-lg text-center transform transition-transform duration-300 hover:scale-105'>
-        <span className='text-2xl font-bold text-gray-800'>Congrats! You have guessed the word!</span>
+    <div className='w-screen z-10 absolute top-0 h-screen flex backdrop-blur-sm bg-opacity-50 bg-[#CDC1FF] justify-center items-center'>
+      <div className=' bg-opacity-30 bg-[#FFD3A3] backdrop-blur-3xl flex flex-col py-24 justify-center items-center p-8 rounded-lg shadow-lg text-center md:transform md:transition-transform md:backdrop:duration-300 md:hover:scale-105'>
+        <span className='text-2xl font-bold text-black'>Congrats! You have guessed the word!</span>
         <button 
           onClick={handlePlayAgain} 
           className='mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-200'
