ac31c93d902bd33c123d2d5cce237521b3a1689f
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-30 07:03:52 +0530

    fix

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -14,7 +14,7 @@ const Letter = ({ letterPos, attemptValue }) => {
 
   return (
     <div
-      className={`text-white cursor-none hover:scale-105 flex items-center justify-center text-2xl lg:text-[40px] w-[40px] h-[40px] lg:w-[70px] lg:h-[70px] my-2 border-[1px] rounded-sm lg:rounded-md border-black
+      className={`text-white cursor-none hover:scale-105 flex transition-colors ease-in 1s items-center justify-center text-2xl lg:text-[40px] w-[40px] h-[40px] lg:w-[70px] lg:h-[70px] my-2 border-[1px] rounded-sm lg:rounded-md border-black
       ${letterState === "correct" ? "bg-green-900" : letterState === "almost" ? "bg-orange-600" : "bg-gray-800"}
       `}
     >
