1bb28f7b390abe15a3698bd47faaf9432905bbc2
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-10-01 17:28:05 +0530

    chore

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -20,8 +20,8 @@ const Key = ({ val }) => {
 
     return (
         <div
-            className={`flex cursor-pointer hover:bg-red-600 items-center rounded-md justify-center font-thin text-[14px] sm:text-[18px] lg:text-2xl text-white bg-gray-600 md:w-[40px] md:h-[40px] w-[30px] h-[30px] text-sm lg:w-16 lg:h-16 flex-wrap 
-            ${val === "enter" || val === "del" ? "w-[60px] md:w-[65px] lg:w-32" : ""}`}
+            className={`flex cursor-pointer  brorder items-center rounded-md justify-center font-thin text-[14px] sm:text-[18px] lg:text-2xl text-white bg-gray-600 md:w-[40px] md:h-[40px] w-[30px] h-[30px] text-sm lg:w-16 lg:h-16 flex-wrap 
+            ${val === "enter" || val === "del" ? "w-[60px] hover:bg-green-800 md:w-[65px] lg:w-32" : "hover:bg-violet-700"}`}
             onClick={() => selectLetter(val)}
         >
             <span>{val.toUpperCase()}</span>
